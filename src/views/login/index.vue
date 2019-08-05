10b3d4048fcf4b1fca122b7168b80e9770ecec69
jinminrui <969172689@qq.com>
2019-08-05 17:07:15 +0800

    2019.8.5

---
+++ src/views/login/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <p class="title">手机号登录</p>
+    <div class="form-wrapper">
+
+    </div>
+  </div>
+</template>
+
+<script type="ts">
+export default {}
+</script>
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .title {
+    font-size: 20px;
+    color: black;
+  }
+  .form-wrapper {
+    height: 5px;
+  }
+}
+</style>
